2af91d38d4d92ac5ab43be66accbaa780b2e2553
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-05-03 21:59:16 +0300

    api test route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 
 
 mongoose
-.connect('mongodb://127.0.0.1:27017/realEstate')
-.then(() => console.log('successfully conntected to mongoDB!'))
-.catch((err)=> console.log(err));
+    .connect('mongodb://127.0.0.1:27017/realEstate')
+    .then(() => console.log('successfully conntected to mongoDB!'))
+    .catch((err) => console.log(err));
 
 const app = express();
 
 app.listen(3000, () => console.log('app listening on port 3000'));
+
+app.use('/api/user', userRouter)
