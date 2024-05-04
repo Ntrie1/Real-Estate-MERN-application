c5a716dd3de712d74dca9dac8638f2a0bea56cb0
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-05-04 20:28:41 +0300

    auth route added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 
 mongoose
@@ -10,6 +11,9 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => console.log('app listening on port 3000'));
 
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter); 
