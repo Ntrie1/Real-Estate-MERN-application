3caae6c64cb17b71133ed2ac8f05a50baaf6216c
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-05-05 22:13:12 +0300

    signin route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
+dotenv.config();
 
 mongoose
     .connect('mongodb://127.0.0.1:27017/realEstate')
@@ -27,5 +28,5 @@ app.use((err, req, res, next) => {
         statusCode,
         message
     });
-    
+
 });
