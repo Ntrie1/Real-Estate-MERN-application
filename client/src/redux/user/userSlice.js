459dc00b38122d4f7c1a9016aa044f71605b4873
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-05-09 16:49:13 +0300

    deleteUser reducers added

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -33,6 +33,18 @@ const userSlice = createSlice({
         updateUserFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
+        },
+        deleteUserStar: (state) =>{
+            state.loading = true;
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
         }
     }
 });
@@ -44,7 +56,10 @@ export const {
     signInFailure,
     updateUserStart,
     updateUserSuccess,
-    updateUserFailure
+    updateUserFailure,
+    deleteUserStar,
+    deleteUserSuccess,
+    deleteUserFailure
 } = userSlice.actions;
 
 export default userSlice.reducer;
