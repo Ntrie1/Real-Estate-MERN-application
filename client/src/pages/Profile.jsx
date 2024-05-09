29eba0c4cf83b3929f239af2eff7900c4db5696d
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-05-09 16:59:11 +0300

    signOut reducers created and implemented

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,7 +8,10 @@ import {
   updateUserSuccess,
   deleteUserFailure,
   deleteUserStar,
-  deleteUserSuccess
+  deleteUserSuccess,
+  signOutUserStar,
+  signOutUserSuccess,
+  signOutUserFailure
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -99,8 +102,24 @@ export default function Profile() {
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
-  }
+  };
 
+  const handleSignOut = async () => {
+    dispatch(signOutUserStar());
+
+    try {
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message))
+      };
+
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message))
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -160,7 +179,7 @@ export default function Profile() {
 
       <div className='flex justify-between mt-5' >
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
 
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
