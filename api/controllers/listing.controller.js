57f1de18e025e6ab378bee525ba5912252d66824
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-05-10 20:01:48 +0300

    update listing routes

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -34,11 +34,11 @@ export const deleteListing = async (req, res, next) => {
 export const updateListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
 
-    if(!listing){
+    if (!listing) {
         return next(errorHandler(404, 'Listing not found!'));
     }
 
-    if(req.user.id !== listing.userRef){
+    if (req.user.id !== listing.userRef) {
         return next(errorHandler(401, 'You can only update your own listings!'))
     }
 
@@ -46,11 +46,23 @@ export const updateListing = async (req, res, next) => {
         const updatedListing = await Listing.findByIdAndUpdate(
             req.params.id,
             req.body,
-            {new: true}
+            { new: true }
         );
         res.status(200).json(updatedListing);
-    } catch (error) { 
+    } catch (error) {
         next(error)
     }
 
+};
+
+export const getListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if(!listing){
+            return next(errorHandler(404, 'Listing not found!'));
+        }
+        res.status(200).json(listing);
+    } catch (error) {
+        next(error)
+    }
 };
